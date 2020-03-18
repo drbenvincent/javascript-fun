aee1745b21cbe625cd608de2839be2455f40256b
Benjamin T. Vincent <drbenvincent@users.noreply.github.com>
2020-03-18 12:50:53 +0000

    bounce logic done :)

--- bouncing balls/canvas.js
+++ bouncing balls/canvas.js
@@ -7,11 +7,13 @@ var context = document.querySelector("canvas").getContext("2d");
 class Ball {
     constructor(x, y, radius) {
         this.color = "#008000";
-        this.direction = Math.random() * Math.PI * 2;
         this.radius = radius;
-        this.speed = Math.random() * 0.5 + 0.1;
+        this.speed = Math.random() * 2 + 0.1;
+        this.direction = Math.random() * Math.PI * 2;
         this.x = x;
         this.y = y;
+        this.dx = Math.cos(this.direction) * this.speed
+        this.dy = Math.sin(this.direction) * this.speed
     }
     draw() {
         context.fillStyle = this.color;
@@ -19,9 +21,25 @@ class Ball {
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
     }
-    move() {
-        this.x += Math.cos(this.direction) * this.speed;
-        this.y += Math.sin(this.direction) * this.speed;
+    move(width, height) {
+        this.x += this.dx;
+        this.y += this.dy;
+        // check collision with left or right
+        if (this.x - this.radius < 0) {
+            this.x = 0 + this.radius
+            this.dx = -this.dx
+        } else if (this.x + this.radius > width) {
+            this.x = width - this.radius
+            this.dx = -this.dx
+        }
+        // check collision with top or bottom
+        if (this.y - this.radius < 0) {
+            this.y = 0 + this.radius
+            this.dy = -this.dy
+        } else if (this.y + this.radius > height) {
+            this.y = height - this.radius
+            this.dy = -this.dy
+        }
     }
 }
 
@@ -37,16 +55,17 @@ var ball = new Ball(100, 100, 5);
 // context.fill();
 //ball.draw()
 
-
+let height = 400
+let width = 600
 
 function loop() {
     window.requestAnimationFrame(loop)
 
     // redraw/wipe canvas
-    context.canvas.height = 400;
-    context.canvas.width = 600;
+    context.canvas.height = height;
+    context.canvas.width = width;
 
-    ball.move()
+    ball.move(width, height)
     ball.draw()
 
     // update text
